526b43cbad3ccb9e49ecc303a388166c2eb3b52c
Seb Gotvitch <83121989+OrmnkeBudBad@users.noreply.github.com>
2022-03-19 10:59:12 +0800

    Update FlutterTtsPlugin.m

--- ios/Classes/FlutterTtsPlugin.m
+++ ios/Classes/FlutterTtsPlugin.m
@@ -1,5 +1,10 @@
 #import "FlutterTtsPlugin.h"
+#if __has_include(<better_player/better_player-Swift.h>)
 #import <flutter_tts/flutter_tts-Swift.h>
+#else
+#import "flutter_tts-Swift.h"
+#endif
+
 
 @implementation FlutterTtsPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
